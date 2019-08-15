f06b516dfc80f43a5f7de8fc2d353e11b457457c
Boris Gromov <0xff.root@gmail.com>
2019-08-15 18:02:31 +0200

    Initial Commit

---
+++ CrazyFlieTestApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CrazyFlieTestApp
+//
+//  Created by 0xff on 15/08/2019.
+//  Copyright © 2019 Volaly. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
